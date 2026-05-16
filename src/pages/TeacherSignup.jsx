67c10555f0d7a77c1b645b34fe5b7d5c043b386f
Rittik Patra <patraarittik1999@gmail.com>
2026-05-16 09:39:03 +0530

    Fixing teacher signup structure as error messages integrates

--- src/pages/TeacherSignup.jsx
+++ src/pages/TeacherSignup.jsx
@@ -110,67 +110,117 @@ const TeacherSignup = () => {
           
           <label htmlFor="name">Teacher Name:</label>
 
-          <input onChange={handleInputChange} name="fullname" value={teacherData.fullname} required type="text" id="name" placeholder="Prof. / Mr. / Ms." autoFocus />
+
+          <div>
+            <input onChange={handleInputChange} name="fullname" value={teacherData.fullname} required type="text" id="name" placeholder="Prof. / Mr. / Ms." autoFocus />
           {errors.fullname && <p>{errors.fullname}</p>}
 
+          </div>
+          
+
           <label htmlFor="qualification">Highest Qualification:</label>
-          <input onChange={handleInputChange} name="hqualification" value={teacherData.hqualification} required type="text" id="qualification" placeholder="e.g. M.Sc Mathematics" />
+          <div>
+            <input onChange={handleInputChange} name="hqualification" value={teacherData.hqualification} required type="text" id="qualification" placeholder="e.g. M.Sc Mathematics" />
+
+          </div>
+          
 
           <label htmlFor="exp">Teaching Experience (Years):</label>
-          <input onChange={handleInputChange} name="experience" value={teacherData.experience} required type="number" id="exp" placeholder="e.g. 5" />
+          <div>
+            <input onChange={handleInputChange} name="experience" value={teacherData.experience} required type="number" id="exp" placeholder="e.g. 5" />
+
+          </div>
+          
 
           <label htmlFor="status">Current Employment:</label>
-          <select onChange={handleInputChange} name="employment" value={teacherData.employment} required id="status">
+          <div>
+            <select onChange={handleInputChange} name="employment" value={teacherData.employment} required id="status">
             <option value="private">Private Tutor</option>
             <option value="govt">Govt. School Teacher</option>
             <option value="freelance">Pvt. School Teacher</option>
             <option value="corporate">Trainer</option>
           </select>
 
+          </div>
+          
           <label htmlFor="subjects">Areas of Expertise:</label>
-          <input onChange={handleInputChange} name="expertise" value={teacherData.expertise} required type="text" id="subjects" placeholder="Math, Science, etc." />
+          <div>
+            <input onChange={handleInputChange} name="expertise" value={teacherData.expertise} required type="text" id="subjects" placeholder="Math, Science, etc." />
+          </div>
+          
 
           <label htmlFor="email">Email:</label>
-          <input onChange={handleInputChange} name="email" value={teacherData.email}  required type="email" id="email" placeholder="email@example.com" />
+          <div>
+            <input onChange={handleInputChange} name="email" value={teacherData.email}  required type="email" id="email" placeholder="email@example.com" />
           <p className="errorText">{errors.email}</p>
+          </div>
+          
 
           <label htmlFor="pass">Password:</label>
-          <input onChange={handleInputChange} name="password" value={teacherData.password} required type="password" id="pass" placeholder="********" />
+
+          <div className="inputGroup" >
+            <input onChange={handleInputChange} name="password" value={teacherData.password} required type="password" id="pass" placeholder="********" />
           {errors.password && <p>{errors.password}</p>}
 
+          </div>
+          
+
           <label htmlFor="pass">Conform Password:</label>
-          <input onChange={handleInputChange} name="conformpassword" value={teacherData.conformpassword} required type="password" id="conformpass" placeholder="********" />
+          <div className="inputGroup">
+            <input onChange={handleInputChange} name="conformpassword" value={teacherData.conformpassword} required type="password" id="conformpass" placeholder="********" />
           {errors.conformpassword && <p>{errors.conformpassword}</p>}
 
+          </div>
+          
+
           <label htmlFor="dob">Date of Birth:</label>
-          <input onChange={handleInputChange} name="dob" value={teacherData.dob} required type="date" id="dob" />
+          <div className="inputGroup">
+            <input onChange={handleInputChange} name="dob" value={teacherData.dob} required type="date" id="dob" />
+          </div>
+          
 
           <label htmlFor="mobile">Mobile Number:</label>
-          <input onChange={handleInputChange} name="phonenumber" value={teacherData.phonenumber} required type="tel" id="mobile" placeholder="+91..." />
+
+          <div className="inputGroup">
+            <input onChange={handleInputChange} name="phonenumber" value={teacherData.phonenumber} required type="tel" id="mobile" placeholder="+91..." />
           {errors.phonenumber && <p>{errors.phonenumber}</p>}
 
+          </div>
+          
+
           <label htmlFor="gender">Gender:</label>
-          <select onChange={handleInputChange} name="gender" value={teacherData.gender} required id="gender">
+
+          <div className="inputGroup">
+            <select onChange={handleInputChange} name="gender" value={teacherData.gender} required id="gender">
             <option value="">Select Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="other">Other</option>
           </select>
 
+          </div>
+          
+
           <label htmlFor="city">City:</label>
-          <input onChange={handleInputChange} name="city" value={teacherData.city} required type="text" id="city" placeholder="e.g. Bhadreswar" />
+          <div className="inputGroup">
+            <input onChange={handleInputChange} name="city" value={teacherData.city} required type="text" id="city" placeholder="e.g. Bhadreswar" />
+          </div>
+          
 
           <label htmlFor="pincode">Pincode:</label>
           <input onChange={handleInputChange} name="pincode" value={teacherData.pincode} required type="text" id="pincode" placeholder="e.g. 712124" />
 
 
           <label htmlFor="bio">Teaching Bio:</label>
-          <textarea onChange={handleInputChange} name="bio" value={teacherData.bio} required
+          <div className="inputGroup">
+            <textarea onChange={handleInputChange} name="bio" value={teacherData.bio} required
             id="bio" 
             placeholder="Tell students about your teaching style..." 
             rows="3" 
             style={{width: '100%', borderRadius: '6px', padding: '8px', border: '1px solid black'}}
           />
+          </div>
+          
 
           <button type="submit" className="submitBtn">Submit</button>
         </form>
